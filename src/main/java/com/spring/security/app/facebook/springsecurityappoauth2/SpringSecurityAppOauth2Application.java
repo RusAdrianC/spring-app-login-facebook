70054265fa65c1bb5c27b63411d9f038f5847243
RusAdrianC <rusadi78@yahoo.com>
2022-06-13 17:07:52 +0300

    first commit

---
+++ src/main/java/com/spring/security/app/facebook/springsecurityappoauth2/SpringSecurityAppOauth2Application.java
@@ -0,0 +1,13 @@
+package com.spring.security.app.facebook.springsecurityappoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAppOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAppOauth2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/security/app/facebook/springsecurityappoauth2/SpringSecurityAppOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.security.app.facebook.springsecurityappoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAppOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
